3fbda856588cef19ff3e1b57a9536c32b2717ca3
Pavel Bezrukov <darud4@yandex.ru>
2022-06-12 10:34:21 +0400

    refctor(cors): move Access-Control-Allow-Credentials to conditions

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -9,12 +9,13 @@ module.exports.handleCors = function handleCors(req, res, next) {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
 
-  res.header('Access-Control-Allow-Credentials', true);
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
